17ef1c8fdc95010979fd613df6eed665ca5f519a
Shane R. Spencer <shane@bogomip.com>
2011-01-02 11:14:54 -0900

    Tweaks

---
+++ django/mac-eater/makemessages.sh
@@ -0,0 +1,2 @@
+PATH=~/Downloads/google_appengine:$PATH ./manage.py makemessages --ignore='django*' --ignore='Geohash' --ignore='static' --ignore='filetransfers' -l en
+PATH=~/Downloads/google_appengine:$PATH ./manage.py makemessages --ignore='django*' --ignore='Geohash' --ignore='static' --ignore='filetransfers' -l de
